7a89afec4665584bb5fc8398c8bbd70f91c7aa25
Hillary Datoc <hitdatoc@gmail.com>
2014-12-07 18:52:47 +0800

    virus ATP (not fully implemented)

--- classes/ATP.py
+++ classes/ATP.py
@@ -4,8 +4,14 @@ from pygame.locals import *
 class ATP(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
+		#Tower ATP
 		self.initATP = 30
 		self.currentATP = self.initATP
+
+		#Virus ATP
+		self.initVirusATP = 30
+		self.currentVirusATP = self.initVirusATP
+		
 		self.basicFont = pygame.font.SysFont(None, 30)
 		self.displayText = str(self.currentATP)
 		self.image = self.basicFont.render(self.displayText, True, (255,255,255))
@@ -15,6 +21,10 @@ class ATP(pygame.sprite.Sprite):
 	def addATP(self, wave):
 		#FUNCTION: ((0.3x)^2)+15
 		self.currentATP += int((0.3*wave)**2)+15
+
+	def addVirusATP(self, wave):
+		#FUNCTION:
+		pass
 
 	def update(self):
 		self.displayText = str(self.currentATP)
