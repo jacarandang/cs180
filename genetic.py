92d0a5dc452f41d5fac09270cbe2bea3e8f9f069
Jym Paul A. Carandang <pauljym_carandang@yahoo.com>
2014-12-07 16:35:58 +0800

    Fitness function

---
+++ genetic.py
@@ -0,0 +1,80 @@
+from random import uniform, random, randint, choice
+
+class Member:
+
+	def __init__(self, values = None):
+		self.value = values
+		if self.value is None: self.value = [uniform(-.5, .5) for i  in xrange(14*6)]
+		self.fitness = 0
+	
+	def mutate(self):
+		for i in xrange(len(self.value)):
+			if random() > 0.5:
+				self.value[i] += uniform(-.1, .1)
+				
+	def evaluate(self):
+		self.fitness = sum(self.value)
+		return self.fitness
+		
+	def __cmp__(self, other):
+		return self.fitness - other.fitness
+		
+	def __repr__(self):
+		return str(self.fitness)
+
+class Genetic:
+
+	def __init__(self, population = None):
+	
+		self.pop = population
+		if self.pop is None: self.pop = [Member() for i in xrange(10)]
+		
+	def crossover(self, a, b):
+		a_pop = a.value
+		b_pop = b.value
+		n_pop = []
+		for i in xrange(len(a_pop)):
+			if randint(0, 1) == 0:
+				n_pop.append(a_pop[i])
+			else:
+				n_pop.append(b_pop[i])
+		m = Member(n_pop)
+		if random() >= 0.05:
+			m.mutate()
+		return m
+				
+	def mutation(self, member):
+		member.mutate()
+		
+	def avg(self, pop):
+		s = sum([p.fitness for p in pop])
+		return s*1.00/len(pop)
+		
+	def start(self, generations = 10):
+		pop = self.pop
+		for i in xrange(generations):
+			for m in pop:
+				m.evaluate()
+			breed = []			#parents for breeding
+			while not len(pop) == 0:
+				a = choice(pop)
+				pop.remove(a)
+				b = choice(pop)
+				pop.remove(b)
+				if a > b:
+					breed.append(a)
+				else:
+					breed.append(b)
+			n_pop = []			#new population
+			for i in xrange(10):
+				a = choice(breed)
+				b = choice(breed)
+				n_pop.append(self.crossover(a, b))
+			pop = n_pop
+			
+		for m in pop:
+			m.evaluate()
+		return pop
+
+g = Genetic()
+pop  = g.start(10)
