b009dcdbd5eac27b10a27886268b44a530b0e08c
Jym Paul A. Carandang <pauljym_carandang@yahoo.com>
2014-12-09 15:58:13 +0800

    intial tower ai

--- classes/towerAI.py
+++ classes/towerAI.py
@@ -3,8 +3,11 @@ from pygame.locals import *
 
 class TowerPlayer:
 
-	def __init__(self, res):
-		pass
+	def __init__(self, res, tlist, grid):
+		self.actions = []
+		self.res = res
+		self.tlist = []
+		self.grid = grid
 		
 	def getActions(self):
 		pass
