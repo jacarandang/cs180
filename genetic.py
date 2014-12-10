b612b5c53afdac49bb34b588c41501dc3b0c30f3
Jym Paul A. Carandang <pauljym_carandang@yahoo.com>
2014-12-10 13:41:14 +0800

    genetic modification

--- genetic.py
+++ genetic.py
@@ -2,21 +2,26 @@ from random import uniform, random, randint, choice
 import pickle
 from main import Game
 import pygame
-import os
+import os, sys, getopt
 from classes.member import Member
 pygame.init()
 
 class Genetic:
 
-	def __init__(self, space = 10, population = None):
+	def __init__(self, space = 10, population = None, action = None):
 	
 		self.pop = population
 		self.space = space
-		if self.pop is None: self.pop = [Member() for i in xrange(space)]
+		if self.pop is None:
+			self.pop = [Member() for i in xrange(space)]
+		self.action = action
+		if self.action == None:
+			self.action = ['actions']
 				
 	def evaluate(self, member):
 		scr = pygame.display.set_mode((800,600))
-		g = Game(scr, member, None, 'actions')
+		ac = choice(self.action)
+		g = Game(scr, member, None, ac)
 		a = g.start()
 		if g.thing.life < 0:
 			g.thing.life = 0
@@ -56,11 +61,51 @@ class Genetic:
 			self.evaluate(m)
 		return pop
 
-dir = 'ai'
-g = Genetic(1)
-pop  = g.start(1)
-pop.sort(reverse = True)
-print "Last population with fitness"
-print pop
-with file(os.path.join(dir, 'data.net'), "wb") as f:
-	pickle.dump(pop, f)
+if __name__ == '__main__':
+	
+	argv = sys.argv[1:]
+	pop = 5
+	gen = 10
+	action = None
+	populations = None
+	outputF = 'data.net'
+	try:
+		opts, args = getopt.getopt(argv,"hp:g:a:n:o:")
+	except getopt.GetoptError:
+		print 'genetic.py	-p [# of population] -g [# of generation] -a [action files] -n [population files] -o [output]'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'genetic.py	-p [# of population] -g [# of generation] -a [action files] -n [population files] -o [output]'
+			sys.exit()
+		if opt == '-p':
+			pop = int(arg)
+		elif opt == '-g':
+			gen = int(arg)
+		elif opt == '-a':
+			action = arg.split(',')
+		elif opt == '-n':
+			populations = arg.split(',')
+		elif opt == '-o':
+			outputF = arg
+	
+	
+	members = []
+	if populations is not None:
+		for p in populations:
+			with file(p, "rb") as f:
+				n = pickle.load(f)
+				for m in n:
+					members.append(m)
+	else:
+		members = None
+		
+	print pop, members, action, gen
+	raw_input()
+	g = Genetic(pop, members, action)
+	pop  = g.start(gen)
+	pop.sort(reverse = True)
+	print "Last population with fitness"
+	print pop
+	with file(os.path.join(dir, outputF), "wb") as f:
+		pickle.dump(pop, f)
